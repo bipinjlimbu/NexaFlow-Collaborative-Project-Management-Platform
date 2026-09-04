b3e7fe61c475bc5c4aacdb4eca9e359b82d553a4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-04 20:29:40 +0545

    Get Tokens For User Added

--- backend/nf_project/nf_app/views/auth_view.py
+++ backend/nf_project/nf_app/views/auth_view.py
@@ -54,3 +54,10 @@ def register_view(request):
         if user is not None:
             return Response({'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
         return Response({'error': 'User registration failed.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
