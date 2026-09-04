982b85bf016381613391532d51446e7859cf483f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-04 20:00:57 +0545

    User Model Added

--- backend/nf_project/nf_app/models.py
+++ backend/nf_project/nf_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
--- backend/nf_project/nf_project/settings.py
+++ backend/nf_project/nf_project/settings.py
@@ -106,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'nf_app.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
