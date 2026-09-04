28f6858e1424bff791c8d1c4e4405bf2db96d677
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-04 20:45:09 +0545

    Workspaces Serializer Added

--- backend/nf_project/nf_app/serializers.py
+++ backend/nf_project/nf_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Workspaces
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+        
+class WorkspaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspaces
         fields = '__all__'
