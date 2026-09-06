99590639729721f9e9a26cb236709aa1d0b73785
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-06 20:25:54 +0545

    Register Auth Service Added

--- frontend/nf-project/src/service/authService.ts
+++ frontend/nf-project/src/service/authService.ts
@@ -12,3 +12,20 @@ export async function login(
         }),
     });
 }
+
+export async function register(data: {
+    username: string;
+    email: string;
+    password: string;
+    confirm_password: string;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    address: string;
+    profile_picture: string;
+}) {
+    return apiFetch("/register/", {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+}
