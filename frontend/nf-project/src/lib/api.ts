d93fe397672b321efaf8410e4ee9fed0c0c554fd
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-06 20:23:01 +0545

    API Fetch Lib Added

---
+++ frontend/nf-project/src/lib/api.ts
@@ -0,0 +1,22 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function apiFetch(
+    endpoint: string,
+    options?: RequestInit
+) {
+    const response = await fetch(`${API_URL}${endpoint}`, {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...options?.headers,
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.error || "Something went wrong");
+    }
+
+    return data;
+}
