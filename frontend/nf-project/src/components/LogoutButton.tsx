e4fa260586f9be1ef2d03e5c336dc6e94a6be9c4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-06 20:53:33 +0545

    Logout Button Component Updated

--- frontend/nf-project/src/components/LogoutButton.tsx
+++ frontend/nf-project/src/components/LogoutButton.tsx
@@ -3,7 +3,13 @@
 import { useRouter } from "next/navigation";
 import { logout } from "@/services/authService";
 
-export default function LogoutButton() {
+type LogoutButtonProps = {
+    onLogout: () => void;
+};
+
+export default function LogoutButton({
+    onLogout,
+}: LogoutButtonProps) {
     const router = useRouter();
 
     async function handleLogout() {
@@ -18,13 +24,16 @@ export default function LogoutButton() {
         localStorage.removeItem("access");
         localStorage.removeItem("refresh");
 
+        onLogout();
+
         router.push("/login");
     }
 
     return (
         <button
+            type="button"
             onClick={handleLogout}
-            className="rounded-lg bg-slate-900 px-4 py-2 text-sm font-medium text-white"
+            className="rounded-lg bg-slate-900 px-4 py-2 text-sm font-medium text-slate-300 hover:bg-slate-800 hover:text-white"
         >
             Logout
         </button>
