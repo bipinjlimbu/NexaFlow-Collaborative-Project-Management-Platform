7ca9bb4cbae90788c926f95a19eb99163e7fe749
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-11 17:55:25 +0545

    Workspace Detail API Url Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main_view import workspaces_view
+from .views.main_view import workspaces_view, workspace_detail_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('workspaces/', workspaces_view, name='workspaces'),
+    path('workspaces/<int:pk>/', workspace_detail_view, name='workspace_detail'),
 ]
