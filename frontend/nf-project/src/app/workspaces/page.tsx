389f825631f75789b2398d8227bb1c666d8e9637
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-13 18:52:25 +0545

    Workspaces Page Updated

--- frontend/nf-project/src/app/workspaces/page.tsx
+++ frontend/nf-project/src/app/workspaces/page.tsx
@@ -130,7 +130,6 @@ export default function WorkspacesPage() {
         setCreateError("");
     };
 
-    console.log("Workspace", workspaces);
     if (isAuthenticated === null || loading) {
         return <WorkspacesSkeleton />;
     }
