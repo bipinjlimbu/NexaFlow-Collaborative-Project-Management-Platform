f458bd551c6a838c3cf85ac53a900619dd8fbdbf
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-13 21:18:01 +0545

    Send Workspace Invitation API Url Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.workspaces_view import workspaces_view, workspace_detail_view, get_users_list
+from .views.workspaces_view import workspaces_view, workspace_detail_view, get_users_list, send_workspace_invitation
 from .views.profile_view import profile_view
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('workspaces/', workspaces_view, name='workspaces'),
     path('workspaces/<int:pk>/', workspace_detail_view, name='workspace_detail'),
     path('users/', get_users_list, name='get_users_list'),
+    path('workspaces/<int:workspace_id>/invite/', send_workspace_invitation, name='send_workspace_invitation'),
     path('profile/', profile_view, name='profile'),
 ]
