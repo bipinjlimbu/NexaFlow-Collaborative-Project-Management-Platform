079145c6ef1d92b02928e9117af498f799945e40
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-14 15:43:23 +0545

    Accept Invitation PATCH API Added

--- backend/nf_project/nf_app/views/invitation_view.py
+++ backend/nf_project/nf_app/views/invitation_view.py
@@ -16,3 +16,20 @@ def get_invitations_view(request):
         return Response({"error": "Invitations not found."}, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def accept_invitation_view(request, pk):
+    try:
+        invitation = WorkspaceInvitation.objects.get(pk=pk, invited_user=request.user)
+    except WorkspaceInvitation.DoesNotExist:
+        return Response({"error": "Invitation not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PATCH':
+        if invitation.status != WorkspaceInvitation.Status.PENDING:
+            return Response({"error": "Invitation is not pending."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        invitation.status = WorkspaceInvitation.Status.ACCEPTED
+        invitation.save()
+        serializer = WorkspaceInvitationSerializer(invitation)
+        return Response(serializer.data, status=status.HTTP_200_OK)
