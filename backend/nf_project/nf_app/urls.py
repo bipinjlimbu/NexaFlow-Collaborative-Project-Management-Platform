20dd2f9df499ec82217d1af79017d0e78c906ef0
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-14 15:05:36 +0545

    Notification Detail API Url Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
 from .views.workspaces_view import workspaces_view, workspace_detail_view, get_users_list, send_workspace_invitation
 from .views.profile_view import profile_view
-from .views.notification_view import get_notifications_view
+from .views.notification_view import get_notifications_view, notification_detail_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -14,4 +14,5 @@ urlpatterns = [
     path('workspaces/<int:workspace_id>/invite/', send_workspace_invitation, name='send_workspace_invitation'),
     path('profile/', profile_view, name='profile'),
     path('notifications/', get_notifications_view, name='get_notifications'),
+    path('notifications/<int:pk>/', notification_detail_view, name='notification_detail'),
 ]
