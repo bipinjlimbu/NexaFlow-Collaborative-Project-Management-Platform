44c7e6d84fef56abfee99642255af3183346f136
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-14 14:49:52 +0545

    GET Notification API Url Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
 from .views.workspaces_view import workspaces_view, workspace_detail_view, get_users_list, send_workspace_invitation
 from .views.profile_view import profile_view
+from .views.notification_view import get_notifications_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('users/', get_users_list, name='get_users_list'),
     path('workspaces/<int:workspace_id>/invite/', send_workspace_invitation, name='send_workspace_invitation'),
     path('profile/', profile_view, name='profile'),
+    path('notifications/', get_notifications_view, name='get_notifications'),
 ]
