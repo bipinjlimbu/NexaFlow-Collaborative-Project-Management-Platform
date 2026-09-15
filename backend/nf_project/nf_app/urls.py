87dd5877140e2fb507ffbf653f040af6f7af3e92
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-15 19:40:08 +0545

    Project Detail API Url Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -4,7 +4,7 @@ from .views.workspaces_view import workspaces_view, workspace_detail_view, get_u
 from .views.profile_view import profile_view
 from .views.notification_view import get_notifications_view, notification_detail_view
 from .views.invitation_view import get_invitations_view, accept_invitation_view, decline_invitation_view
-from .views.project_view import projects_view
+from .views.project_view import projects_view, project_detail_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -25,4 +25,5 @@ urlpatterns = [
     path('workspaces/<int:workspace_id>/members/<int:user_id>/remove/', remove_workspace_member, name='remove_workspace_member'),
     path('workspaces/<int:workspace_id>/projects/', get_projects_in_workspace, name='workspace_projects'),
     path('projects/', projects_view, name='projects'),
+    path('projects/<int:pk>/', project_detail_view, name='project_detail'),
 ]
