d6bce9d06b8f7acf3ef883bbe0a7a415b6b965f7
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-23 18:31:29 +0545

    Project Service Updated

--- frontend/nf-project/src/services/projectService.ts
+++ frontend/nf-project/src/services/projectService.ts
@@ -27,7 +27,7 @@ export async function createProject(
 export async function getProject(
     id: number
 ): Promise<Project> {
-    return authFetch(`/ projects / ${id}/`);
+    return authFetch(`/projects/${id}/`);
 }
 
 export async function updateProject(
