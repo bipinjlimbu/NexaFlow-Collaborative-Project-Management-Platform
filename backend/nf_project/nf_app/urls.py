c3ead1a3a8b8e11f33262fa2a1050518da94af2c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-09-23 18:07:32 +0545

    New Urls Added

--- backend/nf_project/nf_app/urls.py
+++ backend/nf_project/nf_app/urls.py
@@ -5,6 +5,7 @@ from .views.profile_view import profile_view
 from .views.notification_view import get_notifications_view, notification_detail_view
 from .views.invitation_view import get_invitations_view, accept_invitation_view, decline_invitation_view
 from .views.project_view import projects_view, project_detail_view, get_workspace_members, add_member_to_project, get_project_members
+from .views.task_view import tasks_view, task_detail_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -29,4 +30,6 @@ urlpatterns = [
     path('projects/workspace/<int:workspace_id>/members/', get_workspace_members, name='get_workspace_members'),
     path('projects/<int:project_id>/members/<int:user_id>/add/', add_member_to_project, name='add_member_to_project'),
     path('projects/<int:project_id>/members/', get_project_members, name='get_project_members'),
+    path('tasks/', tasks_view, name='tasks'),
+    path('tasks/<int:pk>/', task_detail_view, name='task_detail'),
 ]
